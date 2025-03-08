8d40bc5924f785a40348ea770df2897595402004
Lschwebs <Lschwebs@uwyo.edu>
2025-03-08 11:47:13 -0700

    updates

---
+++ SingleSurveyExample/scripts/R2SingleSurvey.m
@@ -0,0 +1,71 @@
+% Lena J. Schwebs
+% Created on: 09/27/2024
+% Last updated: 10/28/2024
+
+% Execute R2 inversion for Lippmann measurement
+% MUST have: R2.in, protocol.dat, mesh.dat 
+% Workflow: Import and preprocess raw data file, write protocol.dat, 
+% write mesh.dat, write R2.in, start R2 inversion
+
+%% USER DEFINED INPUT
+% data file and preprocessing parameters
+fLoc = 'User specified'; % raw data file
+minVal = 0; % minimum resistance value allowed
+errRecip = 0.05; % reciprocal error threshold in DECIMAL units
+
+% INVERSION parameters
+numel = 4025; % number of elements, first val from mesh file
+reg_mode = 0;    % regularixation mode, need to use 1 for O&L doi calc
+alpha_s = 1;    % regularization parameter, use >1 for O&L
+alpha_aniso = 1; % alphaaniso > 1 for smoother horizontal models
+                 % alphaaniso = 1 for isotropic smoothing
+                 % alphaaniso < 1 for smoother vertical models
+a_wgt = 0.0; % calcualted from measured data errors
+b_wgt = 0.0; % calculate from measured data errors
+num_electrodes = 128;   % number of electrodes in the survey
+elecSep = 1;    % electrode separation in meters
+res_meter = 'User specified'; % Lippmann, SuperSting, DAS1
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% preprocess raw data and write R2.in
+switch res_meter % use specified resistivity meter import function
+            case 'Lippmann'
+                imDat = importLippmann(fLoc); 
+            case 'SuperSting'
+                fprintf('code TBD')
+            case 'DAS1'
+                imDat = importDAS1(fLoc); 
+            otherwise
+                disp('Invalid resistivity meter')
+end
+
+survey_type = 1; % 1 = starting or single survey
+[data, gmean] = preproc_Pwl(fLoc, imDat, minVal, errRecip, survey_type); % preprocess raw data
+startRes = gmean; % geometric mean of apparent resistivities 
+writeR2in(startRes, 1, numel, reg_mode, alpha_s, alpha_aniso, num_electrodes, a_wgt, b_wgt) % write R2.in
+
+%% execute R2
+system('R2.exe')
+
+%% move files
+datfiles = dir([pwd '\*.*_res.dat']);
+vtkfiles = dir([pwd '\*.*_res.vtk']);
+
+for i = 1:length(datfiles)
+    movefile([pwd '\' datfiles(i).name],[pwd '\results\ref\' datfiles(i).name]);
+    movefile([pwd '\' vtkfiles(i).name],[pwd '\results\ref\' vtkfiles(i).name]);
+end
+
+movefile([pwd '\f001_res.dat'],[pwd '\results\f001_res.dat']);
+movefile([pwd '\f001_res.vtk'],[pwd '\results\f001_res.vtk']);
+movefile([pwd '\f001_sen.dat'],[pwd '\results\f001_sen.dat']);
+movefile([pwd '\f001_err.dat'],[pwd '\results\f001_err.dat']);
+movefile([pwd '\R2.out'],[pwd '\results\ref\R2.out']);
+movefile([pwd '\R2.in'],[pwd '\results\ref\R2.in']);
+movefile([pwd '\protocol.dat'],[pwd '\results\ref\protocol.dat']);
+movefile([pwd '\electrodes.dat'],[pwd '\results\ref\electrodes.dat']);
+movefile([pwd '\electrodes.vtk'],[pwd '\results\ref\electrodes.vtk']);
+movefile([pwd '\f001_errMod.png'],[pwd '\results\ref\f001_errMod.png']);
+copyfile([pwd '\mesh.dat'],[pwd '\results\ref\mesh.dat']);
---
+++ TimeLapseExample_MD/scripts/preproc_Pwl.m
@@ -0,0 +1,146 @@
+% Lena J. Schwebs
+% Created on: 10/28/2024
+% Last updated: 03/07/2025
+% Adapted from Dr. Andrew D. Parsekian 'preprocMPT.m'
+
+% USE FOR SINGLE SURVEY OR DATA DIFFERENCING INVERSION WITH POWER LAW ERROR MODEL
+% preprocLippDD imports, removes bad data based on reciprocals, removes
+% NANS, removes negative values 
+% writes Protocol.dat with extra column from initial dataset
+
+% minVal is the smallest R value that will be kept
+% errRecip is the reciprocal error threshold that will be retained in
+% DECIMAL UNITS
+% dataStart is resistance from starting dataset
+
+function [protocolData, gmean] = preprocLippDD(fLoc, data, minVal, errRecip, survey_type, dataStart)
+
+%% import file and create data matrices
+D = data; % load raw data array
+abmn = [D(:,1) D(:,2) D(:,3) D(:,4)]; % takes electrode locations from raw data
+R = D(:,5); % takes resistance from raw data
+rho = D(:,6); % takes apparent resistivity from raw data
+dat = [abmn R rho]; % makes a single matrix out of all raw data (NO ERRORS RIGHT NOW)
+survey_type = survey_type; % switch case for incoporating starting dataset into protocol.dat
+
+%% clean up negative or NaN values
+dat_a = sortrows(dat,5); % sort based on column that will have NaNs
+firstD = max(find(dat_a(:,5) < minVal)) + 1; % finds the last negative val, +1 for first positive value. used to delete negative R vals
+lastD = find(~isnan(dat_a(:,5)), 1, 'last'); % finds the beginning of the NaN rows to delete
+
+dat = dat_a(firstD:lastD,:); % take only rows >0 and without NaN R values
+
+%% loop through all quadrapoles to find reciprocals
+inst = dat;
+
+for i = 1:length(inst)
+    tx = sort(inst(i,1:2)); % AB
+    rx = sort(inst(i,3:4)); % MN
+        
+    for j = 1:length(inst)
+        Tx = sort(inst(j,1:2));
+        Rx = sort(inst(j,3:4));
+            
+        if rx == Tx & tx == Rx
+            reciprocal(i,:) = [tx rx Tx Rx inst(i,5) inst(j,5) inst(i,6) inst(j,6)]; % inst(:, 5) = resistance, inst(:, 6) = apparent resistivity
+        end
+    
+    end
+end
+    
+for i = 1:length(reciprocal)
+    holder = reciprocal(i,:);
+    reciprocal(i,:) = [0 0 0 0 0 0 0 0 0 0 0 0];
+
+    for j = 1:length(reciprocal)
+        if reciprocal(j,1:8) == [holder(5:8) holder(1:4)]
+            reciprocal(j,:) = [0 0 0 0 0 0 0 0 0 0 0 0];    
+        end
+    end
+        
+        reciprocal(i,:) = holder; 
+end
+
+reciprocal = reciprocal(find(reciprocal(:,1)),:);
+reciprocal = [reciprocal(:,1:4) reciprocal(:,9:12)]; % abmn forward(R) reciprocal(R) forward(rho) reciprocal(rho)
+reciprocal(:,9) = abs(reciprocal(:,5) - reciprocal(:,6)); % adds column 9 which is the abs.diff between FWD/RECIP
+
+for i = 1:length(reciprocal)
+    if max(reciprocal(i,1:2)) > max(reciprocal(i,3:4))
+        reciprocal(i,1:4) = [sort(reciprocal(i,3:4),2) sort(reciprocal(i,1:2),2)];
+    else
+        reciprocal(i,1:4) = [sort(reciprocal(i,1:2),2) sort(reciprocal(i,3:4),2)];
+    end  
+end
+
+for R = 1:length(reciprocal)
+    Xr(R) = mean([mean(reciprocal(R,1:2)) mean(reciprocal(R,3:4))]);
+    Zr(R) = abs((max(reciprocal(R,1:2))-min(reciprocal(R,3:4))))+abs(reciprocal(R,1)-reciprocal(R,2));
+end
+  
+RECIPS = [reciprocal Xr' Zr']; % reciprocal electrode locations, Rf, Rr, rhof, rhor, absolute deviation, add on pseudolocations for plotting
+RECIPS = [RECIPS RECIPS(:,9)./mean(RECIPS(:,5:6),2)]; % add on a column of percent reciprocal error in decimal units
+
+%% remove any U% above XX% reciprocal error
+cnt = 1;
+
+for i = 1:length(RECIPS)
+
+    if RECIPS(i,12) < errRecip % reciprocal error
+        DAT(cnt,:) = [RECIPS(i,1:4), mean(RECIPS(i,5:6)), mean(RECIPS(i, 7:8)), RECIPS(i,12)]; % loop through and keep all columns in each row below threshold
+        cnt = cnt+1;
+    end
+end
+
+data = DAT; % abmn, resistance, apparent resistivity, recip error
+ 
+gmean = geomean(data(:, 6)); % geometric mean
+
+fprintf('Percent of Measurements Remaining = %2.2f%% \n', 100 .* length(data) ./ (length(abmn)./2))
+fprintf('Geometric Mean = %2.2f \n', gmean)
+ fprintf('Length data = %2.f\n', length(data))
+%% calculate Power Law Error Model
+P = PwlErrMod(data, fLoc);
+data(:, 8) = 10.^P(2) .* data(:, 5).^P(1);
+
+%% switch case for starting/single survey or time lapse
+if survey_type == 1 % single or starting survey
+    % assemble R2 protocol.dat
+    pro_data = [data(:,1:5) data(:,8)];
+    out = zeros(1,6); % initialize output matrix
+    out = [out; pro_data(:, 1:6)]; % abmn, and resistance (FILTERED DATA)
+    nums = 1:length(out)-1; % create measurement # vector
+    out = [nums' out(2:end,:)]; % add measurement # vector to output array
+    mn = max(nums); % total number of measurements
+    protocolData = out;
+    newfile = [pwd '/protocol.dat']; % create protocol.dat file
+    dlmwrite(newfile, mn); % write first line of protocol.dat
+    dlmwrite(newfile, protocolData, '-append','delimiter','\t'); % write line 2-mn filtered data to protocol.dat
+    clear newfile;
+    fprintf('protocol.dat written\n')
+
+elseif survey_type == 2
+    % find intersection of initial dataset and current dataset
+    [dataN, id, idS] = intersect(data(:, 1:4), dataStart(:, 2:5), 'rows');
+    fprintf('Length data = %2.f\n', length(data))
+    fprintf('Length dataStart = %2.f\n', length(dataStart))
+    % assemble R2 protocol.dat
+    pro_data = [data(id,1:5) dataStart(idS, 5) data(id,8)];
+    fprintf('Length pro_data = %2.f\n', length(pro_data))
+    out = zeros(1,7); % initialize output matrix
+    out = [out; pro_data(:, 1:7)]; % abmn, resistance (FILTERED DATA), starting resistance
+    nums = 1:length(out)-1; % create measurement # vector
+    out = [nums' out(2:end,:)]; % add measurement # vector to output array
+    mn = max(nums); % total number of measurements
+    protocolData = out;
+    newfile = [pwd '/protocol.dat']; % create protocol.dat file
+    dlmwrite(newfile, mn); % write first line of protocol.dat
+    dlmwrite(newfile, protocolData, '-append','delimiter','\t'); % write line 2-mn filtered data to protocol.dat
+    clear newfile;
+
+    fprintf('protocol.dat written\n')
+else
+    fprintf('ERROR WRITING PROTOCOL.DAT\n')
+end
+
+end
